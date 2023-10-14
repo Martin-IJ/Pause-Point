7c51b20ae758da6d6c2cf4a183ec5cb7e4e703ad
Nathan-Yinka <oludarenathaniel@gmail.com>
2023-10-14 16:39:00 +0100

    completed the about page

---
+++ src/compments/Footer.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import logo from "../assets/images/Pause Point 3 1.svg";
+import facebook from "../assets/images/logos_facebook.svg";
+import whatsapp from "../assets/images/logos_whatsapp-icon.svg";
+import call from "../assets/images/ph_phone-call-light.svg";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer style={{ backgroundColor: "rgba(4, 151, 60, 0.40)" }}>
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-12 pt-2 col-md-8">
+            <div className="d-flex flex-column gap-4 gap-md-2 justify-content-center align-items-center justify-content-md-start align-items-md-start">
+              <div className="">
+                <img src={logo} alt="" className="img-fluid" />
+              </div>
+              <p className="col-12 col-md-6 pb-1 text-center text-md-start">
+                Lorem ipsum dolor sit amet consectetur. Consectetur eu nulla
+                lorem neque cras ligula suspendisse. Sit molestie donec porta
+                turpis pharetra a ut amet.
+              </p>
+
+              <div className="d-flex gap-md-3 gap-5 ">
+                <Link to={""}>
+                  <img src={facebook} alt="" />
+                </Link>
+                <Link to={""}>
+                  <img src={whatsapp} alt="" />
+                </Link>
+                <Link to={""}>
+                  <img src={call} alt="" />
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-lg-4 pt-2 d-flex justify-content-between pt-5 pt-lg-0 flex-column flex-lg-row gap-3 gap-lg-0 justify-content-center align-items-center justify-content-md-between align-items-md-start">
+            <div>
+              <h4 className="fs-5 fw-semibold pt-1 pb-3">Information</h4>
+              <ul>
+                <Link to={""}>
+                  <li className="pb-2 text-center text-md-start">F.A.Q</li>
+                </Link>
+                <Link to={""}>
+                  <li className="pb-2 text-center text-md-start">
+                    Privacy Policy
+                  </li>
+                </Link>
+                <Link to={""}>
+                  <li className="pb-2 text-center text-md-start">
+                    Terms of Use
+                  </li>
+                </Link>
+              </ul>
+            </div>
+            <div>
+              <h4 className="fs-5 fw-semibold pt-1 pb-3">Quick Links</h4>
+              <ul>
+                <Link to={""}>
+                  <li className="pb-2 text-center text-md-start">Home</li>
+                </Link>
+                <Link to={""}>
+                  <li className="pb-2 text-center text-md-start">About Us</li>
+                </Link>
+                <Link to={""}>
+                  <li className="pb-2 text-center text-md-start">Services</li>
+                </Link>
+                <Link to={""}>
+                  <li className="pb-2 text-center text-md-start">Contact Us</li>
+                </Link>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,26 +3,121 @@ import "../styles/about.css"
 import hero from "../assets/images/about-header.svg"
 import star2 from "../assets/images/Star 2.svg"
 import star1 from "../assets/images/Star 1.svg"
+import image1 from "../assets/images/Frame 91.svg"
+import image2 from "../assets/images/Frame 8.svg"
+import person1 from '../assets/images/Ellipse 7.svg'
+import person2 from '../assets/images/Ellipse 8.svg'
+import person3 from '../assets/images/Ellipse 9.svg'
+import linkedin from "../assets/images/devicon_linkedin.svg"
+import twitter from "../assets/images/fa6-brands_x-twitter.png"
+import { Link } from 'react-router-dom'
+import Footer from '../compments/Footer'
 
 const About = () => {
   return (
     
     <div className='about-con'>
         <header style={{backgroundColor:"rgba(4, 151, 60, 0.80)"}}>
-        <div className='container d-flex justify-content-between align-items-center about-hero gap-3'>
-            <div className=' w-50 pe-5'>
+        <div className='container-lg d-flex justify-content-between align-items-center about-hero gap-3 position-relative' style={{height:"553px"}}>
+            <div className=' col-12 col-sm-6 pe-lg-5'>
                 <h1 className='text-light fs-2 fw-semibold pb-3'>About Us</h1>
-                <p className='pe-5 fw-light text-light'>
+                <p className='pe-5 fw-normal text-light fs-5 '>
                 Lorem ipsum dolor sit amet consectetur. Adipiscing neque massa et netus ut luctus porta duis in. Lorem ipsum dolor sit amet consectetur. Adipiscing neque massa et netus ut luctus porta duis in.
                 </p>
             </div>
-            <div className='w-50'>
+            <div className='d-none d-sm-block col-sm-6 pe-2'>
                 <img src={hero} alt="" className='img-fluid' />
             </div>
-            <img src={star2} alt="" className='position-absolute' style={{bottom:'50px',left:"14px"}}/>
+            <img src={star2} alt="" className='position-absolute d-none d-sm-block' style={{bottom:'50px',left:"14px"}}/>
             <img src={star1} alt="" className='position-absolute' style={{top:'26px',right:"25px"}}/>
         </div>
         </header>
+        <section style={{backgroundColor:"#FFFDE4"}} className='pt-2 pb-5'>
+            <main className='container-lg pb-5 pe-3 pe-lg-0'>
+            <div className='d-flex align-items-center pt-5 mt-3 gap-5 flex-column flex-md-row'>
+                <div className='col-12 col-md-5'>
+                    <h3 className='fs-3 fw-semibold pb-3 text-center text-md-start'>Vision</h3>
+                    <p  className='fw-normal fs-5'>
+                    Lorem ipsum dolor sit amet consectetur. Orci diam curabitur a congue. Nulla et lobortis vel ut orci iaculis erat cras amet. Senectus ultricies interdum aliquam euismod purus ornare. Blandit risus tincidunt leo ornare. Sed phasellus lectus urna habitant odio amet ultrices molestie. Auctor leo leo vestibulum sit sed fames arcu interdum enim. Semper quis pulvinar venenatis eget lobortis amet. Sit viverra luctus praesent mauris et lectus. Scelerisque massa scelerisque nulla dis vel gravida. Donec lacus nisl suscipit elit consequat mi curabitur egestas. Ornare egestas tortor tincidunt massa elit lacus egestas sem laoreet.
+                    </p>
+                </div>
+                <div className='col-12 col-md-6'>
+                    <img src={image1} alt=""  className='img-fluid' />
+                </div>
+            </div>
+
+            <div className='d-flex align-items-center pt-5 mt-3 gap-5 flex-column-reverse flex-md-row'>
+            <div className='col-12 col-md-6'>
+                    <img src={image2} alt=""  className='img-fluid' />
+                </div>
+                <div className='col-12 col-md-5'>
+                    <h3 className='fs-3 fw-semibold pb-3 text-center text-md-start'>Mission</h3>
+                    <p  className='fw-normal fs-5'>
+                    Lorem ipsum dolor sit amet consectetur. Orci diam curabitur a congue. Nulla et lobortis vel ut orci iaculis erat cras amet. Senectus ultricies interdum aliquam euismod purus ornare. Blandit risus tincidunt leo ornare. Sed phasellus lectus urna habitant odio amet ultrices molestie. Auctor leo leo vestibulum sit sed fames arcu interdum enim. Semper quis pulvinar venenatis eget lobortis amet. Sit viverra luctus praesent mauris et lectus. Scelerisque massa scelerisque nulla dis vel gravida. Donec lacus nisl suscipit elit consequat mi curabitur egestas. Ornare egestas tortor tincidunt massa elit lacus egestas sem laoreet.
+                    </p>
+                </div>
+            </div>
+            <div className='pt-5 mt-3'>
+            <h3 className='fs-3 fw-semibold pb-3 text-center mb-1'>Our Team</h3>
+            <div className='d-flex justify-content-between gap-4 flex-column flex-md-row'>
+                <div className='flex-fill' style={{backgroundColor:"rgba(4, 151, 60, 0.30)"}}>
+                    <div className='d-flex flex-column justify-content-center align-items-center'>
+                        <div style={{width:"100px",height:"100px"}} className='rounded-circle my-3'> 
+                            <img src={person1} alt="" className='img-fluid'/>
+                        </div>
+                        <div className='pb-3'>
+                            <h4 className='text-center fs-5 fw-semibold'>Paul Chopper</h4>
+                            <h5 className='text-center fs-6 fw-medium'>Co-Founder</h5>
+                        </div>
+                    </div>
+                    <div style={{backgroundColor:"#04973C"}}>
+                            <div className='d-flex justify-content-center align-items-center py-2 gap-2'>
+                                <Link to={""}><img src={linkedin} alt="" /></Link>
+                                <Link to={""}><img src={twitter} alt="" /></Link>
+                            </div>
+                    </div>
+                </div>
+
+                <div className='flex-fill' style={{backgroundColor:"rgba(4, 151, 60, 0.30)"}}>
+                    <div className='d-flex flex-column justify-content-center align-items-center'>
+                        <div style={{width:"100px",height:"100px"}} className='rounded-circle my-3'> 
+                            <img src={person2} alt="" className='img-fluid'/>
+                        </div>
+                        <div className='pb-3'>
+                            <h4 className='text-center fs-5 fw-semibold'>Paul Chopper</h4>
+                            <h5 className='text-center fs-6 fw-medium'>Co-Founder</h5>
+                        </div>
+                    </div>
+                    <div style={{backgroundColor:"#04973C"}}>
+                            <div className='d-flex justify-content-center align-items-center py-2 gap-2'>
+                                <Link to={""}><img src={linkedin} alt="" /></Link>
+                                <Link to={""}><img src={twitter} alt="" /></Link>
+                            </div>
+                    </div>
+                </div>
+
+                <div className='flex-fill' style={{backgroundColor:"rgba(4, 151, 60, 0.30)"}}>
+                    <div className='d-flex flex-column justify-content-center align-items-center'>
+                        <div style={{width:"100px",height:"100px"}} className='rounded-circle my-3'> 
+                            <img src={person3} alt="" className='img-fluid'/>
+                        </div>
+                        <div className='pb-3'>
+                            <h4 className='text-center fs-5 fw-semibold'>Paul Chopper</h4>
+                            <h5 className='text-center fs-6 fw-medium'>Co-Founder</h5>
+                        </div>
+                    </div>
+                    <div style={{backgroundColor:"#04973C"}}>
+                            <div className='d-flex justify-content-center align-items-center py-2 gap-2'>
+                                <Link to={""}><img src={linkedin} alt="" /></Link>
+                                <Link to={""}><img src={twitter} alt="" /></Link>
+                            </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            </main>
+        </section>
+        <Footer/>
     </div>
   )
 }
