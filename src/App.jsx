3aa50415fe061a4f3d195475714106e2ac7492e0
martins <martinohez@gmail.com>
2023-10-14 11:51:10 +0100

    Done with the Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Navbar from './pages/Navbar'
 import './styles/App.css'
 
 function App() {
 
   return (
     <div>
-      App
+      <Navbar />
+      <Routes>
+        <Route path='home' element={<Home />} />
+      </Routes>
     </div>
   )
 }
